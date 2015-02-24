9c6484e8993a84b14442ce46679b263ece301116
curtiszimmerman <curtis.zimmerman@gmail.com>
2015-02-23 23:26:21 -0800

    added: unit test files

---
+++ test/app.test.js
@@ -0,0 +1,27 @@
+/**
+ * @project iojs-api-template
+ * API template boilerplate for io.js (es6/harmony)
+ * @file app.test.js
+ * Primary application driver testing harness.
+ * @author curtis zimmerman
+ * @contact software@curtisz.com
+ * @license AGPLv3
+ * @version 0.0.1a
+ */
+
+module.exports = exports = __test = (function() {
+	"use strict";
+
+	var expect = require('chai').expect;
+
+	var app = require('../app.js');
+
+	describe('iojs-api-template (es6/harmoney boilerplate):', function() {
+		describe('# app.stub():', function() {
+			it('should return false when given zero arguments', function() {
+				var result = true;
+				//expect(result).to.be.true();
+			});
+		});
+	});
+})();
