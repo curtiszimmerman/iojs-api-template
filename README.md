eac38cee2953c46725b184fdb427b28a43a1a1aa
curtiszimmerman <curtis.zimmerman@gmail.com>
2015-02-23 23:25:20 -0800

    added: yargs CLI parser (refactor), app() core function wrapper, init function wrapper, README update

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # iojs-api-template
 An API template/boilerplate for io.js and es6/harmony.
+
+## Prerequisites
+This package requires [io.js](https://iojs.org/) and uses special ES6/Harmony flags to run.
+
+## Installing
+```sh
+git clone https://github.com/curtiszimmerman/iojs-api-template.git
+cd iojs-api-template
+npm install
+```
+
+## Executing
+It's possible to run this application using iojs with special flags:
+```sh
+iojs --harmony --harmony_arrow_functions --harmony_classes ./app.js
+```
+
+Additionally, this package includes a simple shell script that serves as an alias:
+```sh
+./iojs-api-template.sh
+```
+
+Help information is available with the `--help` flag:
+```sh
+iojs --harmony --harmony_arrow_functions --harmony_classes ./app.js --help
+```
+... Or ...
+```sh
+./iojs-api-template.sh --help
+```
+
+## Options
+This application supports the following command-line options:
+```sh
+-a, --address [address]      server address to listen on. default localhost.
+-c, --certificate [cert]     SSL certificate to use. implies -k.
+-k, --key [key]              SSL private key to use. implies -c.
+-l, --loglevel [loglevel]    output loglevel (0-5). default 2.
+-p, --port [port]            tcp port to listen on. default 80.
+-v, --version                display version information.
+```
+
+## Contributing
+Contributions are welcome, but some standard guidelines apply. Pull requests that are
+simply stylistic or semantic changes will not be accepted. Pull requests that do not
+conform to existing code style will not be accepted. I am a busy person, and I will
+try my best to get to your pull request, but please understand that it may be some
+time before this happens. If it's urgent (bug/security fix), please drop me a line
+at software -at- curtisz -dot- com. Thanks!
+
+## License
+This source is (C) 2015 curtis zimmerman and released under the AGPLv3. The full  
+license is in the `LICENSE` file, and also at: http://www.gnu.org/licenses/agpl.html
--- app.js
+++ app.js
@@ -12,35 +12,98 @@
 module.exports = exports = __api = (function() {
 	"use strict";
 
+	// third-party modules
+	var events = require('events');
 	var koa = require('koa');
+	var pm2 = require('pm2');
+	var yargs = require('yargs');
 
-	var log = require('lib/log.js').config( 5 );
+	// local modules
+	var log = require('./lib/log.js');
 
-	var app = koa();
-
+	// additional init
+	var $app = koa();
 	var $data = {
+		cache: {},
+		settings: {
+			argv: null,
+			logs: {
+				level: 2
+			}
+		},
 		server: {
+			address: '127.0.0.1',
 			port: 4488
 		}
 	};
+	var $log = null;
+	var $pubsub = new events.EventEmitter();
 
-	app.use(function*(next) {
-		var start = new Date();
-		yield next;
-		var respTime = new Date() - start;
-		this.set('X-Response-Time', respTime+'ms');
-	});
+	var init = (function() {
+		$data.server.argv = yargs
+			.usage('Usage: $0 [OPTIONS]\n\nNote:\nThis application requires the following es6/harmony flags:\n--harmony --harmony_arrow_functions --harmony_classes')
+			.option('a', {
+				alias: 'address',
+				describe: 'server address to listen on. default localhost.',
+				type: 'string'
+			})
+			.option('h', {
+				alias: 'help',
+				describe: 'this help message.'
+			})
+			.option('l', {
+				alias: 'loglevel',
+				describe: 'output loglevel (0-5). default 2.',
+				type: 'number'
+			})
+			.option('p', {
+				alias: 'port',
+				describe: 'tcp port to listen on. default 80.',
+				type: 'number'
+			})
+			.option('v', {
+				alias: 'version',
+				describe: 'display version information.'
+			})
+			.help('help')
+			.version(() => require('./package').version)
+			.epilog('For more information, visit:\n https://github.com/curtiszimmerman/iojs-api-template')
+			.argv;
 
-	app.use(function*(next) {
-		var start = new Date();
-		yield next;
-		var respTime = new Date() - start;
-		log.log('<%s> for [%s] (%s)', this.method, this.url, respTime);
-	});
+		$log = log.config( $data.settings.logs.level );
+	})();
 
-	app.use(function*() {
-		this.body = 'Aloha universe!';
-	});
+	var server = function() {
+		var handler = function* (next) {
+			// handle req
+		};
 
-	app.listen( $data.server.port );
+		$app.use(function*(next) {
+			var start = new Date();
+			yield next;
+			var respTime = new Date() - start;
+			this.set('X-Response-Time', respTime+'ms');
+		});
+
+		$app.use(function*(next) {
+			var start = new Date();
+			yield next;
+			var respTime = new Date() - start;
+			$log.log('<'+this.method+'> for ['+this.url+'] ('+respTime+'ms)');
+		});
+
+		$app.use(function*() {
+			this.body = {'Aloha': 'universe!'};
+			this.status = 200;
+			this.type = 'json';
+		});
+
+		$app.on('error', err => $log.error('server error: '+err));
+
+		$app.listen( $data.server.port, $data.server.address );
+		$log.log('listening on '+$data.server.address+':'+$data.server.port);
+	};
+
+	// if we're called as a module, it's testing time!
+	return require.main === module ? server() : this;
 })();
